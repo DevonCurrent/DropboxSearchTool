92f0dd494c00714a4da3a1d740f2a83c3112ff2f
DevonCurrent <dacurrent@bsu.edu>
2019-02-24 20:37:58 -0500

    Added test that checks if file is returned based on FileNameSearch. Fixed issue related to searching by both company and year which returned dropbox folders in the fileList.

--- Tests/TestFileNameSearch.py
+++ Tests/TestFileNameSearch.py
@@ -1,6 +1,7 @@
 import unittest
 import warnings
 import sys, os
+import pdb
 
 runPath = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(runPath, ".."))
@@ -11,13 +12,21 @@ from Message import Message
 from Search import Search
 from ParseMessage import parse_message
 
-class TestWeights(unittest.TestCase):
+class TestFileNameSearch(unittest.TestCase):
     
-    def test_filename_weights_1(self):
-        if __name__ == "__main__":
+    def test_file_name_search_1(self):
+        msg = Message("-fn money -c ibm -y 2016", "N/A", "N/A", "N/A") # there is only one file called 'Money.docx' in this directory
 
-            slackToken = slack
-            dropboxToken = dropbox
+        search = parse_message(dropboxBot, msg)
+        fileList = search.dropbox_search(dropboxBot)
+        fileName = fileList[0].name
 
-            dropboxBot = DropboxBot(dropboxToken)
-            unittest.main()
+        self.assertEqual(fileName, "Money.docx")
+
+if __name__ == "__main__":
+    os.chdir('..')
+    
+    slackBot = SlackBot()
+    dropboxBot = DropboxBot()
+
+    unittest.main()
